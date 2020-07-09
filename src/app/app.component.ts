4e35d0affe64f8b4968f3e40545e44a18781b439
Tahmid79 <45458895+Tahmid79@users.noreply.github.com>
2020-07-09 23:10:24 +0800

    g1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
 @Component({
+
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
 })
+
+
 export class AppComponent {
-  title = 'pages';
+
+  currentPage = 3 ;
+
+ images = [
+    {
+      title : 'At the beach' ,
+      url : 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&auto=format&fit=crop&w=753&q=80'
+    },
+    {
+      title : 'Car ' ,
+      url : 'https://images.unsplash.com/photo-1568605117036-5fe5e7bab0b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60'
+    } ,
+    {
+      title : 'Thunder' ,
+      url : 'https://images.unsplash.com/photo-1537210249814-b9a10a161ae4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60'
+    },
+    {
+      title : 'Another Car' ,
+      url : 'https://images.unsplash.com/photo-1504215680853-026ed2a45def?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60'
+    }
+  ] ;
 }

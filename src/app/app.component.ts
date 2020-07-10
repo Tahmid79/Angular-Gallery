26a3a5670d7978372e5809521aa00f85ec1cd49f
Tahmid79 <45458895+Tahmid79@users.noreply.github.com>
2020-07-10 21:30:38 +0800

    For Directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
 
-  currentPage = 3 ;
+  currentPage = 1 ;
 
  images = [
     {
@@ -29,6 +29,51 @@ export class AppComponent {
     {
       title : 'Another Car' ,
       url : 'https://images.unsplash.com/photo-1504215680853-026ed2a45def?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60'
-    }
+    },
+
+   {
+     title : 'At the beach' ,
+     url : 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&auto=format&fit=crop&w=753&q=80'
+   },
+   {
+     title : 'Car ' ,
+     url : 'https://images.unsplash.com/photo-1568605117036-5fe5e7bab0b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60'
+   } ,
+   {
+     title : 'Thunder' ,
+     url : 'https://images.unsplash.com/photo-1537210249814-b9a10a161ae4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60'
+   },
+   {
+     title : 'Another Car' ,
+     url : 'https://images.unsplash.com/photo-1504215680853-026ed2a45def?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60'
+   },
+
+   {
+     title : 'At the beach' ,
+     url : 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&auto=format&fit=crop&w=753&q=80'
+   },
+   {
+     title : 'Car ' ,
+     url : 'https://images.unsplash.com/photo-1568605117036-5fe5e7bab0b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60'
+   } ,
+   {
+     title : 'Thunder' ,
+     url : 'https://images.unsplash.com/photo-1537210249814-b9a10a161ae4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60'
+   },
+   {
+     title : 'Another Car' ,
+     url : 'https://images.unsplash.com/photo-1504215680853-026ed2a45def?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60'
+   }
+
+
   ] ;
+
+
+ checkWindowIndex( index : number ){
+
+   return Math.abs(this.currentPage -  index ) < 5 ;
+
+ }
+
+
 }

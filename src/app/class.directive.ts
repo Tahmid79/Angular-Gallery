2096957d5460ccef09fb26a25b8985ab847fb589
Tahmid79 <45458895+Tahmid79@users.noreply.github.com>
2020-07-14 16:48:26 +0800

    Custom Times Directive

--- src/app/class.directive.ts
+++ src/app/class.directive.ts
@@ -5,14 +5,20 @@ import { Directive , ElementRef , Input  } from '@angular/core';
 })
 export class ClassDirective {
 
-  //@Input() backgroundColor : string ;
 
   constructor(private element : ElementRef) {}
 
 
+    @Input('appClass') set classNames(classObj : any ){
+       for (let key in classObj){
 
-    @Input() set backgroundColor(color : string){
-      this.element.nativeElement.style.backgroundColor = color;
+            if (classObj[key]){
+              this.element.nativeElement.classList.add(key) ;
+            } else{
+              this.element.nativeElement.classList.remove(key) ;
+            }
+       }
+
     }
 
 
